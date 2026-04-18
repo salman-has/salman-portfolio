701fbb2c539f979ba0e3590e854d28b8203f4b54
Poonam <poonam@gmail.com>
2026-04-18 17:21:59 +0530

    inventory project git added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,36 +1,37 @@
-import capture_netflix from './Capture-netflix-template.png';
-import capture_nit_card from './Capture-nit-card-tamplate.png';
-import shopper_temp from './shopper-screenshot.png';
-import counter_app from './counter_app.png';
-const mywork_data=[
-    {
-        w_no:1,
-        w_desc:"Todo Appication appointment list management",
-        w_img:todo_dash,
-        w_live_url:"https://salman-has.github.io/todo/",
-        w_github_url:"https://github.com/salman-has/todo"
-    },
-     {
-        w_no:2,
-        w_desc:"nit card  template design",
-        w_img:capture_nit_card,
-        w_live_url:"",
-        w_github_url:""
-    },
-     {
-        w_no:3,
-        w_desc:"Sopper template design",
-        w_img:shopper_temp,
-        w_live_url:"",
-        w_github_url:""
-    },
-    {
-         w_no:4,
-         w_desc:"Number Counter App in React",
-         w_img:counter_app,
-         w_live_url:"https://counter-reactvite-app.netlify.app/",
-         w_github_url:"https://github.com/salman-has/react-counter-app-demo-test-.git"
-    }
-
-]
-export default mywork_data
+import capture_netflix from "./Capture-netflix-template.png";
+import capture_nit_card from "./Capture-nit-card-tamplate.png";
+import shopper_temp from "./shopper-screenshot.png";
+import counter_app from "./counter_app.png";
+import inventory_system from '"./inventory_system.png';
+const mywork_data = [
+  {
+    w_no: 1,
+    w_desc: "Todo Appication appointment list management",
+    w_img: todo_dash,
+    w_live_url: "https://salman-has.github.io/todo/",
+    w_github_url: "https://github.com/salman-has/todo",
+  },
+  {
+    w_no: 2,
+    w_desc: "Invenoty System (fullstack)",
+    w_img: inventory_system,
+    w_live_url: "https://github.com/salman-has/inventory-project.git",
+    w_github_url: "https://github.com/salman-has/inventory-project.git",
+  },
+  {
+    w_no: 3,
+    w_desc: "Sopper template design",
+    w_img: shopper_temp,
+    w_live_url: "",
+    w_github_url: "",
+  },
+  {
+    w_no: 4,
+    w_desc: "Number Counter App in React",
+    w_img: counter_app,
+    w_live_url: "https://counter-reactvite-app.netlify.app/",
+    w_github_url:
+      "https://github.com/salman-has/react-counter-app-demo-test-.git",
+  },
+];
+export default mywork_data;
